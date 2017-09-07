f2393529b6956832487f1a1bcc3655cb44cb1238
David Weber <dpweberza@gmail.com>
2017-09-07 23:48:55 +1000

    Structure changes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
-import './App.css';
-
-const logo = require('./logo.svg');
+import 'components/App.css';
+import logo from 'assets/images/logo.svg';
 
 class App extends React.Component {
   render() {
